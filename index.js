2e6129f6227c2a680125a0b5d1af96d491133913
nagimuddin <97009292+nagimuddin@users.noreply.github.com>
2022-05-30 05:19:17 +0600

    update post api

--- index.js
+++ index.js
@@ -1,82 +1,106 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const cors = require('cors');
-const jwt = require('jsonwebtoken');
-require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 // Middleware
 app.use(express.json());
 app.use(cors());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lcx9rts.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 function verifyJWT(req, res, next) {
-    const authHeader = req.headers.authorization;
-    if (!authHeader) {
-        return res.status(401).send({ message: 'UnAuthorized access' });
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "UnAuthorized access" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden access" });
     }
-    const token = authHeader.split(' ')[1];
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
-        if (err) {
-            return res.status(403).send({ message: 'Forbidden access' })
-        }
-        req.decoded = decoded;
-        next();
-    });
+    req.decoded = decoded;
+    next();
+  });
 }
 
 async function run() {
-    try {
-        await client.connect();
-        console.log('Connected to MongoDB');
-        const toolsCollection = client.db('manufacturer').collection('products');
-        const reviewCollection = client.db('manufacturer').collection('reviews');
-        const userCollection = client.db('manufacturer').collection('users');
-        const orderCollection = client.db('manufacturer').collection('orders');
-        const paymentCollection = client.db('manufacturer').collection('payments');
+  try {
+    await client.connect();
+    console.log("Connected to MongoDB");
+    const toolsCollection = client.db("manufacturer").collection("products");
+    const reviewCollection = client.db("manufacturer").collection("reviews");
+    const userCollection = client.db("manufacturer").collection("users");
+    const orderCollection = client.db("manufacturer").collection("orders");
+    const paymentCollection = client.db("manufacturer").collection("payments");
 
-        const verifyAdmin = async (req, res, next) => {
-            const requester = req.decoded.email;
-            const requesterAccount = await userCollection.findOne({ email: requester });
-            if (requesterAccount.role === 'admin') {
-                next();
-            }
-            else {
-                res.status(403).send({ message: 'forbidden' });
-            }
-        }
+    const verifyAdmin = async (req, res, next) => {
+      const requester = req.decoded.email;
+      const requesterAccount = await userCollection.findOne({
+        email: requester,
+      });
+      if (requesterAccount.role === "admin") {
+        next();
+      } else {
+        res.status(403).send({ message: "forbidden" });
+      }
+    };
 
-        // JWT Authentication
-        app.post('/login', (req, res) => {
-            const user = req.body;
-            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-                expiresIn: '1d'
-            });
-            res.send({ accessToken });
-        });
+    // JWT Authentication
+    app.post("/login", (req, res) => {
+      const user = req.body;
+      const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1d",
+      });
+      res.send({ accessToken });
+    });
 
-        // Get Tools
-        app.get('/tools', async (req, res) => {
-            const query = {};
-            const tools = await toolsCollection.find(query).toArray();
-            res.send(tools);
-        })
+    // Get Tools
+    app.get("/tools", async (req, res) => {
+      const query = {};
+      const tools = await toolsCollection.find(query).toArray();
+      res.send(tools);
+    });
 
-        
+    // Get Tools
+    app.post("/tools", async (req, res) => {
+      const tool = req.body;
+      const result = await toolsCollection.insertOne(tool);
+      res.send({ success: true });
+    });
 
-    }
-    finally { }
+    //delete Tools
+    app.delete("/tools/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await toolsCollection.deleteOne({ _id: ObjectId(id) });
+      res.send({ success: true });
+    });
+
+    //single service
+    app.get("/tools/:id", async (req, res) => {
+      const { id } = req.params;
+      const query = { _id: ObjectId(id) };
+      const result = await toolsCollection.findOne(query);
+      res.send(result);
+    });
+  } finally {
+  }
 }
 run().catch(console.dir);
 
-app.get('/', (req, res) => {
-    res.send('Running Server');
-})
+app.get("/", (req, res) => {
+  res.send("Running Server");
+});
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-})
+  console.log(`Server running on port ${port}`);
+});
